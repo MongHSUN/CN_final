e3bbd0ebed3285ec8e1094ec8f3522ff821b5d2e
DearLinny <b02902045@ntu.edu.tw>
2015-12-23 11:21:04 +0800

    multi-thread & pass socket as argument, no I/O

--- server.java
+++ server.java
@@ -1,27 +1,50 @@
 import java.io.*;
 import java.net.*;
-
 public class server{
-	public static void main(String args[]) throws IOException{
+	
+	public class MyRunnable implements Runnable{
+		private Socket client;
+		public MyRunnable(Socket client){
+			System.out.println("Here is the starting point of Thread.");
+			this.client = client;		
+		}
+		public void run(){
+			System.out.println("Run!!");
+			System.out.println(client);
+			//while(true){
+        		//IO
+        		//InputStreamReader isr = new InputStreamReader(client.getInputStream());
+				//BufferedReader br = new BufferedReader(isr);
+				//String str = br.readLine();
+				//System.out.println(str);
+        	//}
+			
+		}
+    }
+
+    public static void main(String args[]) throws IOException{
+		new server().go();		
+	}
+	public void go() throws IOException{
 		//initial
 		int port = 12345;
-		InetAddress addr = InetAddress.getByName("114.45.62.156");
+		InetAddress addr = InetAddress.getByName("10.129.162.32");
 		ServerSocket ser = new ServerSocket(port, 50, addr);
 		//accept
-		Socket client = ser.accept();
-		OutputStream os = client.getOutputStream();
-		PrintWriter pw = new PrintWriter(os, true);
-		pw.println("Your are connected!");
-		InputStreamReader isr = new InputStreamReader(client.getInputStream());
-		BufferedReader br = new BufferedReader(isr);
-		String str = br.readLine();
-		System.out.println(str);
-		//second
-		Socket client2 = ser.accept();
-		OutputStream os2 = client2.getOutputStream();
-		PrintWriter pw2 = new PrintWriter(os2, true);
-		pw2.println("Your are connected!");
-		pw2.println(str);
-		while(true){}
+		while(true){
+			System.out.println("AAA");
+			Socket client = ser.accept();
+			OutputStream os = client.getOutputStream();
+			PrintWriter pw = new PrintWriter(os, true);
+			pw.println("Your are connected!");
+			/*InputStreamReader isr = new InputStreamReader(client.getInputStream());
+			BufferedReader br = new BufferedReader(isr);
+			String str = br.readLine();
+			System.out.println(str);*/
+			//create thread
+			Thread t = new Thread(new MyRunnable(client));
+			t.start();
+			System.out.println("BBB");
+		}
 	}
 }
